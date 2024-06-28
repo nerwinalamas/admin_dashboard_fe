d687e125cef8fde72ee31aeef43aa8d2f81e735d
nerwinalamas <nerwinalamas@gmail.com>
2024-06-29 07:29:42 +0800

    refactor: setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 
 const App = () => {
     return (
-        <div className="w-screen max-w-screen-2xl mx-auto flex gap-2">
-            <div className="hidden w-80 bg-slate-700"></div>
-            <Dashboard />
+        <div className="max-w-screen-2xl mx-auto font-poppins">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                </Routes>
+            </Router>
         </div>
     );
 };
