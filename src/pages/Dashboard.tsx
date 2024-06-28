f8e240bcfed6601b45d2ca341f619aaff00da8a5
nerwinalamas <nerwinalamas@gmail.com>
2024-06-29 07:31:21 +0800

    styles && refactor: dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,29 +1,284 @@
+import { ArrowDown, ArrowUp } from "lucide-react";
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    Tooltip,
+    Legend,
+    ResponsiveContainer,
+    PieChart,
+    Pie,
+    Cell,
+    AreaChart,
+    Area,
+} from "recharts";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "../components/ui/table";
+import {
+    COLORS,
+    salesData,
+    revenueData,
+    visitorsData,
+    stocksData,
+    salesPerCategory,
+    products,
+} from "../assets/data";
+
 const Dashboard = () => {
     return (
-        <div className="w-full p-5 mx-auto grid grid-cols-1 md:grid-cols-2 md:grid-rows-12 lg:grid-cols-4 lg:grid-rows-9 xl:grid-cols-4 xl:grid-rows-5 gap-4">
-            <div className="border-2 border-red-600 lg:col-span-2 xl:col-span-1">
-                Sales
+        <div className="w-full p-5 mx-auto gap-4 grid grid-cols-1 grid-rows-12 md:grid-cols-2 md:grid-rows-10 lg:grid-cols-4 lg:grid-rows-6 xl:grid-rows-5 bg-gray-200">
+            <div className="flex flex-col gap-4 p-4 border shadow-md rounded-lg bg-white lg:col-span-2 xl:col-span-1">
+                <h2 className="text-lg font-semibold text-gray-500">Sales</h2>
+                <div className="flex justify-between">
+                    <div className="flex flex-col justify-center gap-2">
+                        <p className="text-3xl font-bold">250</p>
+                        <div className="flex items-center gap-1 font-medium text-green-600">
+                            <p className="text-sm">+35%</p>
+                            <ArrowUp size={18} strokeWidth={1} />
+                        </div>
+                    </div>
+                    <div className="w-1/2">
+                        <ResponsiveContainer width="100%" height="100%">
+                            <LineChart data={salesData}>
+                                <Line
+                                    type="monotone"
+                                    dataKey="pv"
+                                    stroke="#8884d8"
+                                    strokeWidth={2}
+                                />
+                                <Tooltip />
+                            </LineChart>
+                        </ResponsiveContainer>
+                    </div>
+                </div>
             </div>
-            <div className="border-2 border-red-600 lg:col-span-2 xl:col-span-1">
-                Revenue
+            <div className="flex flex-col gap-4 p-4 border shadow-md rounded-lg bg-white lg:col-span-2 xl:col-span-1">
+                <h2 className="text-lg font-semibold text-gray-500">Revenue</h2>
+                <div className="flex justify-between">
+                    <div className="flex flex-col justify-center gap-2">
+                        <p className="text-3xl font-bold">1500</p>
+                        <div className="flex items-center gap-1 font-medium text-green-600">
+                            <p className="text-sm">+50%</p>
+                            <ArrowUp size={18} strokeWidth={1} />
+                        </div>
+                    </div>
+                    <div className="w-1/2">
+                        <ResponsiveContainer width="100%" height="100%">
+                            <LineChart data={revenueData}>
+                                <Line
+                                    type="monotone"
+                                    dataKey="pv"
+                                    stroke="#8884d8"
+                                    strokeWidth={2}
+                                />
+                                <Tooltip />
+                            </LineChart>
+                        </ResponsiveContainer>
+                    </div>
+                </div>
             </div>
-            <div className="border-2 border-red-600 lg:col-span-2 xl:col-span-1">
-                Visitors
+            <div className="flex flex-col gap-4 p-4 border shadow-md rounded-lg bg-white lg:col-span-2 xl:col-span-1">
+                <h2 className="text-lg font-semibold text-gray-500">
+                    Visitors
+                </h2>
+                <div className="flex justify-between">
+                    <div className="flex flex-col justify-center gap-2">
+                        <p className="text-3xl font-bold">530</p>
+                        <div className="flex items-center gap-1 font-medium text-red-600">
+                            <p className="text-sm">-5%</p>
+                            <ArrowDown size={18} strokeWidth={1} />
+                        </div>
+                    </div>
+                    <div className="w-1/2">
+                        <ResponsiveContainer width="100%" height="100%">
+                            <LineChart data={visitorsData}>
+                                <Line
+                                    type="monotone"
+                                    dataKey="pv"
+                                    stroke="#8884d8"
+                                    strokeWidth={2}
+                                />
+                                <Tooltip />
+                            </LineChart>
+                        </ResponsiveContainer>
+                    </div>
+                </div>
             </div>
-            <div className="border-2 border-red-600 lg:col-span-2 xl:col-span-1">
-                Stocks
+            <div className="flex flex-col gap-4 p-4 border shadow-md rounded-lg bg-white lg:col-span-2 xl:col-span-1">
+                <h2 className="text-lg font-semibold text-gray-500">Stocks</h2>
+                <div className="flex justify-between">
+                    <div className="flex flex-col justify-center gap-2">
+                        <p className="text-3xl font-bold">84</p>
+                        <div className="flex items-center gap-1 font-medium text-green-600">
+                            <p className="text-sm">+10%</p>
+                            <ArrowUp size={18} strokeWidth={1} />
+                        </div>
+                    </div>
+                    <div className="w-1/2">
+                        <ResponsiveContainer width="100%" height="100%">
+                            <LineChart data={stocksData}>
+                                <Line
+                                    type="monotone"
+                                    dataKey="pv"
+                                    stroke="#8884d8"
+                                    strokeWidth={2}
+                                />
+                                <Tooltip />
+                            </LineChart>
+                        </ResponsiveContainer>
+                    </div>
+                </div>
             </div>
-            <div className="border-2 border-red-600 md:col-span-2 md:row-span-2 lg:col-span-4 lg:row-span-2 xl:col-span-3 xl:row-span-2">
-                Revenue Overview Bar Chart
+            <div className="flex flex-col gap-4 p-4 border shadow-md rounded-lg bg-white row-span-2 md:col-span-2 md:row-span-2 lg:col-span-2 lg:row-span-2 xl:col-span-3 xl:row-span-2">
+                <h2 className="text-lg font-semibold text-gray-500">
+                    Revenue Overview
+                </h2>
+                <div className="w-full h-full">
+                    <ResponsiveContainer width="100%" height="100%">
+                        <AreaChart
+                            width={200}
+                            height={60}
+                            data={revenueData}
+                            margin={{
+                                top: 5,
+                                right: 0,
+                                left: 0,
+                                bottom: 5,
+                            }}
+                        >
+                            <Area
+                                type="monotone"
+                                dataKey="uv"
+                                stackId="1"
+                                stroke="#8884d8"
+                                fill="#8884d8"
+                            />
+                            <Area
+                                type="monotone"
+                                dataKey="pv"
+                                stackId="1"
+                                stroke="#82ca9d"
+                                fill="#82ca9d"
+                            />
+                            <Area
+                                type="monotone"
+                                dataKey="amt"
+                                stackId="1"
+                                stroke="#ffc658"
+                                fill="#ffc658"
+                            />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                        </AreaChart>
+                    </ResponsiveContainer>
+                </div>
             </div>
-            <div className="border-2 border-red-600 md:col-span-2 md:row-span-2 lg:col-span-4 lg:row-span-2 xl:col-span-1">
-                Sale by Category Pie Chart
+            <div className="flex flex-col gap-4 p-4 border shadow-md rounded-lg bg-white row-span-2 md:col-span-2 md:row-span-2 lg:col-span-2 lg:row-span-2 xl:col-span-1">
+                <h2 className="text-lg font-semibold text-gray-500">
+                    Sale by Category
+                </h2>
+                <div className="w-full h-full">
+                    <ResponsiveContainer width="100%" height="100%">
+                        <PieChart width={100} height={400}>
+                            <Pie
+                                data={salesPerCategory}
+                                innerRadius={60}
+                                outerRadius={80}
+                                fill="#8884d8"
+                                paddingAngle={5}
+                                dataKey="value"
+                            >
+                                {salesPerCategory.map((entry, index) => (
+                                    <Cell
+                                        key={`cell-${index}`}
+                                        fill={COLORS[index % COLORS.length]}
+                                    />
+                                ))}
+                            </Pie>
+                            <Legend verticalAlign="bottom" />
+                            <Tooltip />
+                        </PieChart>
+                    </ResponsiveContainer>
+                </div>
             </div>
-            <div className="border-2 border-red-600 md:col-span-2 md:row-span-3 lg:col-span-2 lg:row-span-3 xl:col-span-1 xl:row-span-2">
-                Top Products Table
+            <div className="flex flex-col gap-4 p-4 border shadow-md rounded-lg bg-white row-span-2 md:col-span-2">
+                <h2 className="text-lg font-semibold text-gray-500">
+                    Top Products
+                </h2>
+                <div className="w-full h-full">
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>Product</TableHead>
+                                <TableHead>Amount</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {products.map((product) => (
+                                <TableRow key={product.name}>
+                                    <TableCell className="font-medium">{product.name}</TableCell>
+                                    <TableCell>{product.totalAmount}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </div>
             </div>
-            <div className="border-2 border-red-600 md:col-span-2 md:row-span-3 lg:col-span-2 lg:row-span-3 xl:col-span-3 xl:row-span-2">
-                Recent Sales Table
+            <div className="flex flex-col gap-4 p-4 border shadow-md rounded-lg bg-white row-span-2 md:col-span-2">
+                <h2 className="text-lg font-semibold text-gray-500">
+                    Recent Sales
+                </h2>
+                <div className="w-full h-full">
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>
+                                    Product
+                                </TableHead>
+                                <TableHead>Status</TableHead>
+                                <TableHead>Method</TableHead>
+                                <TableHead>
+                                    Amount
+                                </TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {products.map((product) => (
+                                <TableRow key={product.name}>
+                                    <TableCell className="font-medium">
+                                        {product.name}
+                                    </TableCell>
+                                    <TableCell>
+                                        {product.paymentStatus}
+                                    </TableCell>
+                                    <TableCell>
+                                        {product.paymentMethod}
+                                    </TableCell>
+                                    <TableCell>
+                                        {product.totalAmount}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                        <TableFooter>
+                            <TableRow>
+                                <TableCell colSpan={3}>Total</TableCell>
+                                <TableCell>
+                                    85,000.00
+                                </TableCell>
+                            </TableRow>
+                        </TableFooter>
+                    </Table>
+                </div>
             </div>
         </div>
     );
