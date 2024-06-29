9c80ae8d2b05fb1935fa3fa749dac863a98de2da
nerwinalamas <nerwinalamas@gmail.com>
2024-06-29 13:34:15 +0800

    feat: tasks page

---
+++ src/pages/Tasks.tsx
@@ -0,0 +1,13 @@
+const Tasks = () => {
+    return (
+        <div className="w-full h-full p-5 grid grid-cols-1 grid-rows-1">
+            <div className="flex items-center justify-center rounded-lg col-span-1 row-span-1 bg-customCard">
+                <h2 className="text-lg font-semibold text-customPrimary">
+                    Tasks
+                </h2>
+            </div>
+        </div>
+    );
+};
+
+export default Tasks;
