2d07a5d3f24b5218c5e2c6b676cdfa7c4a9efc7e
nerwinalamas <nerwinalamas@gmail.com>
2024-06-29 13:34:51 +0800

    feat: manage users page

---
+++ src/pages/ManageUsers.tsx
@@ -0,0 +1,13 @@
+const ManageUsers = () => {
+    return (
+        <div className="w-full h-full p-5 grid grid-cols-1 grid-rows-1">
+            <div className="flex items-center justify-center rounded-lg col-span-1 row-span-1 bg-customCard">
+                <h2 className="text-lg font-semibold text-customPrimary">
+                    Manage Users
+                </h2>
+            </div>
+        </div>
+    );
+};
+
+export default ManageUsers;
