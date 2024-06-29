62cdecf6147656964448a547ee932e978513e01f
nerwinalamas <nerwinalamas@gmail.com>
2024-06-29 13:32:09 +0800

    feat: assets for theme

---
+++ src/assets/images/index.ts
@@ -0,0 +1,7 @@
+export { default as light } from "./light.png"
+export { default as lightGreen } from "./light_green.png"
+export { default as lightGray } from "./light_gray.png"
+export { default as dark } from "./dark.png"
+export { default as darkBlue } from "./dark_blue.png"
+export { default as darkGray } from "./dark_gray.png"
+export { default as darkPurple } from "./dark_purple.png"
