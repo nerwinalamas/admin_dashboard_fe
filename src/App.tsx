047199935673c601730edbc9862185749ef651ba
nerwinalamas <nerwinalamas@gmail.com>
2024-06-29 13:37:51 +0800

    refactor: setup layout and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,59 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Outlet,
+} from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Analytics from "./pages/Analytics";
+import ManageUsers from "./pages/ManageUsers";
+import Messages from "./pages/Messages";
+import Calendar from "./pages/Calendar";
+import Tasks from "./pages/Tasks";
+import Settings from "./pages/Settings";
+import useThemeStore from "./hooks/useTheme";
+import { useEffect } from "react";
+import Logout from "./pages/Logout";
+
+const Layout = () => {
+    return (
+        <div className="flex min-h-[1000px]">
+            <Sidebar />
+            <div className="flex-1 flex flex-col">
+                <Navbar />
+                <main className="flex-grow">
+                    <Outlet />
+                </main>
+                <Footer />
+            </div>
+        </div>
+    );
+};
 
 const App = () => {
+    const { theme } = useThemeStore();
+
+    useEffect(() => {
+        document.body.className = theme;
+    }, [theme]);
+
     return (
-        <div className="max-w-screen-2xl mx-auto font-poppins">
+        <div className="max-w-screen-2xl mx-auto font-poppins text-customPrimary">
             <Router>
                 <Routes>
-                    <Route path="/" element={<Dashboard />} />
+                    <Route element={<Layout />}>
+                        <Route path="/" element={<Dashboard />} />
+                        <Route path="/analytics" element={<Analytics />} />
+                        <Route path="/manage-users" element={<ManageUsers />} />
+                        <Route path="/messages" element={<Messages />} />
+                        <Route path="/calendar" element={<Calendar />} />
+                        <Route path="/tasks" element={<Tasks />} />
+                        <Route path="/settings" element={<Settings />} />
+                        <Route path="/logout" element={<Logout />} />
+                    </Route>
                 </Routes>
             </Router>
         </div>
