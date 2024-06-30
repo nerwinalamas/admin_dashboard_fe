574dce6467c3c808770b2416daecee58dca7d9ba
nerwinalamas <nerwinalamas@gmail.com>
2024-06-30 10:54:48 +0800

    feat: add user type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,14 @@
-
 export type SidebarItem = {
     id: string;
     label: string;
     icon: JSX.Element;
     url: string;
-}
+};
+
+export type User = {
+    id: string;
+    name: string;
+    email: string;
+    contactNumber: string;
+    address: string;
+};
